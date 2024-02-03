fa4bae83b93c576d8bdaf652d4b03e3103ec0b93
mosidrum <mosiokanga@gmail.com>
2024-02-03 18:59:10 +0100

    setup linters and path mapping

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@src': path.resolve(__dirname, 'src'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+		},
+	},
+});
